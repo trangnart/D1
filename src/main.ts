9f6cdb456ec17240075687230901a7edde3d156f
Jane Tran <ttran161@ucsc.edu>
2023-10-12 13:19:39 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import "./style.css";
+import { setupCounter } from "./counter.ts";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
 <div>
     <div class="card">
-        <button id="counter" type="button">Click 🍭</button>
+        <button id="counter" type="button"></button>
     </div>
 </div>
 `;
@@ -16,3 +17,5 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
